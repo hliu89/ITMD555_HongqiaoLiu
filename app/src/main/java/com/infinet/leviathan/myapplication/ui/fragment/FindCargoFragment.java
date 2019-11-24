5e9a1d5b555d9837b044d7d878c6587cdb8ac778
hliu89 <32824758+hliu89@users.noreply.github.com>
2019-11-24 01:35:17 -0600

    update ui and gradle

---
+++ app/src/main/java/com/infinet/leviathan/myapplication/ui/fragment/FindCargoFragment.java
@@ -0,0 +1,42 @@
+package com.infinet.leviathan.myapplication.ui.fragment;
+
+import android.view.View;
+
+import com.xk.trucktrade.R;
+import com.xk.trucktrade.ui.adapter.CargoInfoAdapter;
+
+/**
+ * Created by xk on 2016/6/2 20:05.
+ */
+public class FindCargoFragment extends TabTopRefreshBaseFragment {
+    @Override
+    protected void setLayoutRes() {
+        layoutRes = R.layout.fragment_findcargo;
+    }
+
+    @Override
+    protected void findViews(View v) {
+        super.findViews(v);
+    }
+
+    @Override
+    protected void setupViews(View v) {
+        super.setupViews(v);     
+        rv_list.setAdapter(new CargoInfoAdapter(getContext()));
+
+    }
+
+    @Override
+    protected void setListener(View v) {
+        super.setListener(v);
+
+    }
+
+    @Override
+    protected void fetchData(View v) {
+        super.fetchData(v);
+    }
+
+
+
+}
